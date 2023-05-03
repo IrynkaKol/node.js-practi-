b253ad41baf7d8f8fd532405d9f2489957ba5616
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-05-03 20:03:46 +0200

    update

---
+++ example-socket.io-chat/frontend/src/App.js
@@ -0,0 +1,21 @@
+import Chat from "./components/Chat/Chat";
+import ChatForm from "./components/ChatForm/ChatForm";
+import SigninChatForm from "./components/SigninChatForm/SigninChatForm";
+
+import './App.css';
+
+function App () {
+    return (
+        <div className="App">
+            <SigninChatForm/>
+            <ChatForm />
+            <Chat/>
+
+
+
+        </div>
+    )
+
+}
+
+export default App;
