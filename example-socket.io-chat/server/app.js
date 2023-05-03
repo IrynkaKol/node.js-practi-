317be5cb474e7fd3550fca1fb1124f7de51d5bbb
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-05-03 19:05:17 +0200

    update

--- example-socket.io-chat/server/app.js
+++ example-socket.io-chat/server/app.js
@@ -2,3 +2,16 @@ const {Server} = require("socket.io");
 const {createServer} = require("http") // підходить для даної задачі
 
 const httpServer = createServer() // створюємо сервер
+
+// створюємо webcoker server на основі httpServer і дозволити підключення всім
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("New frontend connect")
+})
+
+httpServer.listen(3001);
